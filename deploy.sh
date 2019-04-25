50459de7922f924e15fc22f3ede57f228ef78f9f
hzjanger <1305696249@qq.com>
2019-04-25 14:17:26 +0800

    编译脚本

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hzjanger/vueStudy.git master:gh-pages
+git push -f git@github.com:hzjanger/jsStudy.git master:gh-pages
 
 cd -
